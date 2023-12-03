8a9300ae36e8f2888e081f2809d9807b42d9857b
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-03 16:33:07 -0500

    set environments

---
+++ prod/provider.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.57.0"
+    }
+  }
+}
+
+terraform {
+  backend "gcs" {
+      bucket = var.bucket_name_state
+      region = var.location_bucket_tf
+      key    = var.key_terraform
+      prefix = var.environment
+  }
+}
+
+provider "google" {
+  project     = "${var.project_id}-${var.project_name}-${var.organization}-${var.environment}"
+}
+
+terraform {
+  cloud {
+    organization = "mcti-group"
+
+    workspaces {
+      name = "firestore-disaster-recovery"
+    }
+  }
+}

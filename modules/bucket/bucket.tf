b79a963ba6ba7b1b4399c62b8b54e7b03a5c2d35
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-03 16:32:41 -0500

    add module structure

---
+++ modules/bucket/bucket.tf
@@ -0,0 +1,38 @@
+# Create new storage bucket in the northamerica-northeast1 region
+# with standard storage
+# https://cloud.google.com/storage/docs/terraform-create-bucket-upload-object
+
+resource "google_storage_bucket" "capstone2-mcti-bucket" {
+                   
+  name          = "${var.project_id}-${var.project_name}-${var.organization}-${var.bucket_name}-${var.environment}"
+  location      = "northamerica-northeast1"
+  project       = "${var.project_id}-${var.project_name}-${var.organization}-${var.environment}"
+  storage_class = var.storage_class
+  uniform_bucket_level_access = var.uniform_bucket_level_access
+
+  retention_policy {
+    # 10 days retention in seconds
+    retention_period = var.retention_period
+  }
+
+  lifecycle_rule {
+    condition {
+      # days  
+      age > var.lifecycle_rule_age_coldline
+    }
+    action {
+      type = var.lifecycle_rule_set_storage_type
+      storage_class = var.lifecycle_rule_set_storage_class
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      # days  
+      age >= var.lifecycle_rule_age_deletion
+    }
+    action {
+      type = var.lifecycle_rule_set_deletion
+    }
+  }
+}
---
+++ modules/bucket/variables.tf
@@ -0,0 +1,41 @@
+# id project 
+variable "project_id" {}
+
+# name project 
+variable "project_name" {}
+
+# organization name
+variable "organization" {}
+
+# environment name
+variable "environment" {}
+
+# region
+variable "region" {}
+
+# storage class
+variable "storage_class" {}
+
+# uniform bucket-level access to a bucket  
+variable "uniform_bucket_level_access" {}
+
+# retention policy (seconds)
+variable "retention_period" {} 
+
+# lifecycle rule condition
+variables "lifecycle_rule_age_coldline" {}
+
+# lifecycle rule action 
+variables "lifecycle_rule_set_storage_type" {}
+
+# lifecycle rule action 
+variables "lifecycle_rule_set_storage_class" {}
+
+# lifecycle rule condition
+variables "lifecycle_rule_age_deletion" {}
+
+# lifecycle rule action 
+variables "lifecycle_rule_set_deletion" {}
+
+# bucket name
+variables "bucket_name" {}

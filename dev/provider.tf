315c7c0062c6dd79e5ff759e8c161de750ffd7f0
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-03 17:19:16 -0500

    set backend provider

--- dev/provider.tf
+++ dev/provider.tf
@@ -5,19 +5,17 @@ terraform {
       version = "4.57.0"
     }
   }
+    
+  backend "gcs" {
+    bucket = var.bucket_name_state
+    region = var.location_bucket_tf
+    key    = var.key_terraform
+    prefix = var.environment
+  }
 }
 
 provider "google" {
   project     = "${var.project_id}-${var.project_name}-${var.organization}-${var.environment}"
-}
-
-terraform {
-  backend "gcs" {
-      bucket = var.bucket_name_state
-      region = var.location_bucket_tf
-      key    = var.key_terraform
-      prefix = var.environment
-  }
 }
 
 terraform {
--- test/provider.tf
+++ test/provider.tf
@@ -7,26 +7,16 @@ terraform {
   }
 }
 
-terraform {
-  backend "gcs" {
-      bucket = var.bucket_name_state
-      region = var.location_bucket_tf
-      key    = var.key_terraform
-      prefix = var.environment
-  }
-}
-
 provider "google" {
   project     = "${var.project_id}-${var.project_name}-${var.organization}-${var.environment}"
 }
 
 
 terraform {
-  cloud {
-    organization = "mcti-group"
-
-    workspaces {
-      name = "firestore-disaster-recovery"
-    }
+  backend "gcs" {
+    bucket = var.bucket_name_state
+    region = var.location_bucket_tf
+    key    = var.key_terraform
+    prefix = var.environment
   }
 }

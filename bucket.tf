9298f20026c7c4c6b48f91a9ca9e072fb5e509c8
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-03 16:37:19 -0500

    set location bucket

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "bucket_tf_state" {
     name = var.bucket_name_state
-    location = var.region
+    location = var.location_bucket_tf
        
     versioning = var.versioning
 }

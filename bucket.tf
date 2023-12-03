a8a1cea3bdccbef7ffce996bd8565cf9d87585f0
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-03 16:41:20 -0500

    set versioning block

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,8 @@
 resource "google_storage_bucket" "bucket_tf_state" {
     name = var.bucket_name_state
     location = var.location_bucket_tf
-       
-    versioning = var.versioning
+
+    versioning {
+      enabled = var.versioning
+    }
 }

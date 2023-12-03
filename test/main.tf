7c4932b14a4a1b845941e55f641e8c15a0f9c03f
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-03 18:51:04 -0500

    missing arguments

--- test/main.tf
+++ test/main.tf
@@ -1,12 +1,3 @@
 module "bucket" {
   source = "../modules/bucket"
-  
-
-  name = "${var.project_name}-${var.organization}-${var.name_bucket}-${var.environment}"
-  location                      =   var.region
-  project                       =   "${var.project_id}-${var.project_name}-${var.organization}-${var.environment}"
-  storage_class                 =   var.storage_class
-  uniform_bucket_level_access   =   var.uniform_bucket_level_access
-  retention_period              =   var.retention_period
-
 }

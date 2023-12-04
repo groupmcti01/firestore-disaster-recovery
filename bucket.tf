43ded44a4187acee159d7db5709483513e9df4f8
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-04 00:08:14 -0500

    fixing content

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "bucket_tf_state" {
     name = var.bucket_name_state
     location = var.location_bucket_tf
-    project  = "${var.project_id}-${var.organization}-${var.project_name}-${var.environment}"
+    project  = "${var.organization}-${var.project_name}-${var.environment}"
 
     versioning {
       enabled = var.versioning

8d6289ad58585eb6f349a438bd1b2c22c5d92a57
mcti <dcoronado@funio.com>
2023-12-09 21:23:29 -0500

    Try to activate IAM API

--- modules/workflows_schedule/main.tf
+++ modules/workflows_schedule/main.tf
@@ -11,6 +11,11 @@ resource "google_project_service" "project" {
   project = var.project_id
   service = "iam.googleapis.com"
 
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
   disable_dependent_services = true
 }
 
